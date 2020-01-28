9ec7d50c58d098e5e8a9237b3a412a507e12d502
Enrique Alvarez Pereira <enrique.alvarez@imatia.com>
2020-01-28 11:36:58 +0100

    Works successfully

--- build.bat
+++ build.bat
@@ -1,4 +1,4 @@
 cd src/main/resources/webapp
 if not exist node_modules (call npm install) else (echo node_modules already exists.)
 call npm run production-aot
-ren "dist" "static" & move "static" "%~dp0/boot/src/main/resources/webapp/"
+ren "dist" "app" & move "app" "%~dp0/boot/src/main/resources/public/"
