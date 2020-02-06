03664aa6fc2777bf7aee7b74bf891e6800883402
Enrique Alvarez Pereira <enrique.alvarez@imatia.com>
2020-02-06 12:10:20 +0100

    Fixed bugs

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
-set publicUrl = %~dp0/frontend/src/main/resources/public/
+set publicUrl = %~dp0frontend\src\main\resources\public
 cd src/main/resources/webapp
 if not exist node_modules call npm install else echo node_modules already exists.
 call npm run production-aot
-if not exist md %publicUrl%
-ren "dist" "app" & move "app" %publicUrl%
+if not exist %publicUrl% md %publicUrl% else echo public folder already exists.
+ren "dist" "app" & move "app" "%publicUrl%"
