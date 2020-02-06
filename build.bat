d41767d1c5e25e6969017bb2d43cbf2230c245cf
Enrique Alvarez Pereira <enrique.alvarez@imatia.com>
2020-02-06 11:46:15 +0100

    Optimized script

--- build.bat
+++ build.bat
@@ -1,4 +1,6 @@
+set publicUrl = %~dp0/frontend/src/main/resources/public/
 cd src/main/resources/webapp
-if not exist node_modules (call npm install) else (echo node_modules already exists.)
+if not exist node_modules call npm install else echo node_modules already exists.
 call npm run production-aot
-ren "dist" "app" & move "app" "%~dp0/frontend/src/main/resources/public/"
+if not exist md %publicUrl%
+ren "dist" "app" & move "app" %publicUrl%
